1245fadb01cd10affd5ea9b9bdaa43347870e4e6
Zulkar-Jahin <zulkarjahinnizu2000@gmail.com>
2024-06-16 13:28:27 +0600

    initial commit
    This is my (zulkar) first commit to this project.
    
    *Created a connection with local database.
    *Completed Create/Category page.
    *Created a account and got license from syncfusion.com
    
    Everything is working fine till now. (#2173)

---
+++ Expense Tracker/Models/Category.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Expense_Tracker.Models
+{
+    public class Category
+    {
+        [Key]
+        public int CategoryId { get; set; } //primary key
+
+        [Column(TypeName = "nvarchar(50)")]
+        public string Title { get; set; }  //maybe for naming the expense
+
+        [Column(TypeName = "nvarchar(5)")]
+        public string Icon { get; set; } = "";  //this property is used for icons of category
+
+        [Column(TypeName = "nvarchar(10)")]
+        public string Type { get; set; } = "Expense";  //meaning type is income or expense.
+                                                       //Most of the case it will be expense-type.
+                                                       //So initialized with default type="expense"
+        
+
+    }
+}
